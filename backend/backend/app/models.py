7ed524aa7991cf6d9d98801a51822c2c34626441
fxnusky <fanny.maria.aranda@gmail.com>
2025-03-24 14:13:48 +0100

    db: Added google_id to the User model.

--- backend/backend/app/models.py
+++ backend/backend/app/models.py
@@ -1,8 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from app.database import Base
+from .database import Base
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    google_id = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
+    name = Column(String)
--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,10 +1,10 @@
--- db/init.sql
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
+    google_id VARCHAR(100) UNIQUE NOT NULL,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL
 );
 
-INSERT INTO users (name, email) VALUES
-('John Doe', 'john@example.com'),
-('Jane Smith', 'jane@example.com');
+INSERT INTO users (google_id, name, email) VALUES
+('google-id-123', 'John Doe', 'john@example.com'),
+('google-id-456', 'Jane Smith', 'jane@example.com');
