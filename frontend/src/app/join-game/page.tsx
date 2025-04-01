58595b5b363c173a783bce50d7e657bd89b27d0c
fxnusky <fanny.maria.aranda@gmail.com>
2025-04-01 14:45:55 +0200

    fe: Created join-game page.

--- frontend/src/app/join-game/page.tsx
+++ frontend/src/app/join-game/page.tsx
@@ -1,15 +1,50 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import styles from '../page.module.css'
 
 export default function JoinGame() {
   const router = useRouter();
 
   return (
-    <div>
-      <button onClick={() => router.push('/profile')}>
-        Profile
-      </button>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles.cardContent}>
+          <h1 className={styles.title}>Join a game</h1>
+          <input
+            type="text"
+            placeholder="Room code"
+            className={styles.inputField}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Your nickname"
+            className={styles.inputField}
+            required
+          />
+          <button 
+            onClick={() => router.push("/profile")}
+            className={styles.primaryButton}
+            style={{ width: '100%' }}
+          >
+            Join Room
+          </button>
+        </div>
+      </div>   
+      <div className={styles.card}>
+        <div className={styles.cardContent}>
+          
+          <p className={styles.createGameText}>Want to create your own game?</p>
+          
+          <button 
+            onClick={() => router.push("/profile")}
+            className={styles.secondaryButton}
+          >
+            Enter the application
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
