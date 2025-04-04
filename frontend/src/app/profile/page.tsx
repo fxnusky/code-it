6776b4bad4095fe8c5315534f27488287cafd943
fxnusky <fanny.maria.aranda@gmail.com>
2025-04-04 15:19:06 +0200

    fe: Redirect to game page only if the ws connection is correct.

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -3,20 +3,21 @@ import { RequireAuth } from "../../../components/require_auth";
 import styles from '../page.module.css';
 import { useRouter } from 'next/navigation';
 import { connectionService } from '../../../services/ws_connection.service';
-import { useState } from 'react';
+import { useAuth } from "../../../contexts/auth_context";
 import { useWSConnection } from "../../../contexts/ws_connection_context";
 
 export default function Profile() {
   const router = useRouter();
   const connectionService = useWSConnection();
 
+  const {token} = useAuth();
   
       const handleMessage = (message: string) => {
         console.log('Received game message:', message);
       };
       const connect = async () => {
         try {
-            await connectionService.manager_connect();
+            await connectionService.manager_connect(token);
             
             connectionService.addMessageHandler(handleMessage);
             
@@ -24,9 +25,22 @@ export default function Profile() {
             console.error('Connection failed:', error);
         }
       };
-      function handleStartGame(){
-          connect();
-          router.push("/game-manager")
+      async function handleStartGame(){
+          connect(); 
+          
+          await new Promise((resolve, reject) => {
+              const checkConnection = () => {
+                  if (connectionService.getConnectionState() === "open") {
+                      resolve(true);
+                  } else if (connectionService.getConnectionState() === "closed") {
+                      reject(new Error("Connection failed"));
+                  } else {
+                      setTimeout(checkConnection, 100);
+                  }
+              };
+              checkConnection();
+          });
+          router.push("/game-manager");
       }
 
   return (
