7bd789681425e1d7fff9dcce0f986a90472fe80d
fxnusky <fanny.maria.aranda@gmail.com>
2025-04-16 11:21:28 +0200

    fe: Called code execution endpoint when the run button is pressed.

--- frontend/endpoints.config.ts
+++ frontend/endpoints.config.ts
@@ -1,5 +1,6 @@
 const endpoint = {
     dbURL: process.env.NEXT_PUBLIC_API_DB_URL || '', 
+    isolateURL: process.env.NEXT_PUBLIC_ISOLATE_API_DB_URL || '', 
     wsURL: process.env.NEXT_PUBLIC_WS_API_DB_URL || '', 
   };
   
